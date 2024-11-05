cd91c8c193ee833f860a7f9be71e5a3f1ce6b9ca
samquocdoan <samquocdoan.me@gmail.com>
2024-11-05 09:24:12 +0700

    Custom design Quill Editor

--- src/views/posts/detail.php
+++ src/views/posts/detail.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <base href="http://localhost/">
+    <link href="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/css.css">
     <link rel="stylesheet" href="assets/css/post_detail.css">
     <title><?php echo $postData['title'] ?> | iForum</title>
@@ -51,10 +52,31 @@
                     </div>
                 </div>
             </div>
-            <div class="comment-container">
-                <div class="write-comment">
-                    <input type="text" name="" id="" placeholder="Binh Luan">
-                    <button>Binh Luan</button>
+            <p class="title-large">Bình luận</p>
+            <div class="comment-container column-16">
+                <form class="write-comment">
+                    <div id="editor"></div>
+                    <div class="row-8">
+                        <button class="btn-cancel title-small action" disabled>Hủy</button>
+                        <button class="btn-comment title-small action" disabled>Xuất bản</button>
+                    </div>
+                </form>
+
+                <p class="title-medium">Danh sách bình luận [69]</p>
+                <div class="sort-container row-space-between">
+                    <div class="sort-origin row-16">
+                        <a href="tags/<?= $currentTagData['name'] ?>/newest/1" class="sort action<?php if ($sort === 'newest'): ?> active<?php endif ?>">Mới nhất</a>
+                        <a href="tags/<?= $currentTagData['name'] ?>/oldest/1" class="sort action<?php if ($sort === 'oldest'): ?> active<?php endif ?>">Cũ nhất</a>
+                        <a href="tags/<?= $currentTagData['name'] ?>/popularity/week/1" class="sort action<?php if ($sort === 'popularity'): ?> active<?php endif ?>">Nổi bật</a>
+                    </div>
+                    <?php if ($sort === 'popularity'): ?>
+                        <div class="time-frame row-16">
+                            <a href="tags/<?= $currentTagData['name'] ?>/popularity/week/1" class="sort action<?php if ($timeFrame === 'week'): ?> active<?php endif ?>">Tuần</a>
+                            <a href="tags/<?= $currentTagData['name'] ?>/popularity/month/1" class="sort action<?php if ($timeFrame === 'month'): ?> active<?php endif ?>">Tháng</a>
+                            <a href="tags/<?= $currentTagData['name'] ?>/popularity/year/1" class="sort action<?php if ($timeFrame === 'year'): ?> active<?php endif ?>">Năm</a>
+                            <a href="tags/<?= $currentTagData['name'] ?>/popularity/infinite/1" class="sort action<?php if ($timeFrame === 'infinite'): ?> active<?php endif ?>">Tất cả</a>
+                        </div>
+                    <?php endif ?>
                 </div>
                 <div class="comment-list">
                     <?php foreach ($comments as $comment) {
@@ -65,6 +87,12 @@
         </div>
     </main>
     <?php include_once __DIR__ . '/../layouts/footer.php'; ?>
+    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.js"></script>
+    <script>
+        const quill = new Quill('#editor', {
+            theme: 'snow'
+        });
+    </script>
 </body>
 
 </html>
